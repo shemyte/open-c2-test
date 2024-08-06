1fda2a906b2fa29c45a6542350c2f3e29ef8db89
shemyte <shemyte>
2024-08-06 09:52:18 +0300

    Clarification for users

--- lib/openc_c2_test_web/live/run_script_live.ex
+++ lib/openc_c2_test_web/live/run_script_live.ex
@@ -13,43 +13,65 @@ defmodule OpencC2TestWeb.RunScriptLive do
 
   def render(assigns) do
     ~H"""
-    <div class="container mx-auto">
-      <.form class="mt-6 w-1/2 mx-auto" for={@form} phx-change="validate" phx-submit="save">
-        <.input
-          type="select"
-          field={@form[:device]}
-          options={[TwinklyMaha: "twinklymaha"]}
-          prompt="Select project"
-          label="Which device are you testing?"
-        />
+    <.flash_group flash={@flash} />
+    <div class="left-[40rem] fixed inset-y-0 right-0 z-0 hidden lg:block xl:left-[50rem] mt-20">
+      <div class="container mx-auto">
+        <.form class="mt-20 w-3/4" for={@form} phx-change="validate" phx-submit="save">
+          <div class="mb-4">
+            <.input
+            type="select"
+            field={@form[:device]}
+            options={[TwinklyMaha: "twinklymaha"]}
+            prompt="Select project"
+            label="Which device are you testing?"
+            />
+          </div>
 
-        <.input
-          type="select"
-          field={@form[:broker]}
-          options={[
-            emqx_broker: "emqx",
-            HiveMQ_broker: "hivemq"
-          ]}
-          prompt="Select broker"
-          label="Which broker do you want to use?"
-        />
+          <div class="mb-3">
+            <.input
+            type="select"
+            field={@form[:broker]}
+            options={[
+              emqx_broker: "emqx",
+              HiveMQ_broker: "hivemq"
+            ]}
+            prompt="Select broker"
+            label="Which broker do you want to use?"
+            />
+          </div>
 
-        <.input
-          type="select"
-          field={@form[:command]}
-          options={[
-            Turn_led_on: "turn_led_on",
-            Turn_led_off: "turn_led_off",
-            Query_profile: "query_profile",
-            Query_sbom: "query_sbom"
-          ]}
-          prompt="Select command"
-          label="What command do you want to send?"
-        />
-        <div class="mt-2">
-          <.button type="submit" class="bg-indigo-500  w-full">Run</.button>
-        </div>
-      </.form>
+          <div class="mb-3">
+            <.input
+            type="select"
+            field={@form[:command]}
+            options={[
+              Turn_led_on: "turn_led_on",
+              Turn_led_off: "turn_led_off",
+              Query_profile: "query_profile",
+              Query_sbom: "query_sbom"
+            ]}
+            prompt="Select command"
+            label="What command do you want to send?"
+            />
+          </div>
+
+          <div class="mt-2">
+            <.button type="submit" class="bg-indigo-500 w-full">Run</.button>
+          </div>
+        </.form>
+      </div>
+    </div>
+
+    <div class="px-4 py-10 sm:px-6 sm:py-28 lg:px-8 xl:px-28 xl:py-32 mt-20">
+      <div class="mx-auto max-w-xl lg:mx-0">
+        <p class="text-[2rem] mt-4 font-semibold text-indigo-900">
+          Welcome to the new interface for TwinklyMaha.
+        </p>
+        <p class="mt-4 text-indigo-600">
+          To ensure that you have a smooth experience,
+          we created this as your primary way to interact with TwinklyMaha.
+        </p>
+      </div>
     </div>
     """
   end
@@ -65,12 +87,12 @@ defmodule OpencC2TestWeb.RunScriptLive do
 
   def handle_event("save", %{"test_script" => params}, socket) do
     Logger.info("Button Clicked")
-
+    
     publish_message(params)
 
     Logger.info("message is published")
 
-    {:noreply, socket}
+    {:noreply, put_flash(socket, :info, "Successful")}
   end
 
   defp publish_message(%{"command" => "query_profile"}) do
